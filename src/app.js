97fff822bbdc269bb1a06fccca64f271835972a1
Shashwat dubey <68542817+shashwat292001@users.noreply.github.com>
2022-03-20 18:39:50 +0530

    shashwat dubey 19bce10407

--- src/app.js
+++ src/app.js
@@ -1,53 +1,71 @@
-//Progression 1 - create a Manager array and return it
-let managerName = "Alex Ferguson";
-let managerAge = 78;
-let currentTeam = "Manchester FC";
-let trophiesWon = 27;
-
-//Write your function here
-
-// Don't edit the following code
-try {
-  var manager = createManager(
-    managerName,
-    managerAge,
-    currentTeam,
-    trophiesWon
-  );
-} catch (e) {
-  // do nothing - expected error
+//Progression 1:
+//We've got some basic info about Karen's home
+//Debug the type of data provided
+//Return the types concatenated in a single variable
+function moreAboutHome(address, distanceFromTown, hasNeighbours){
+    return typeof(address)+typeof(distanceFromTown)+typeof(hasNeighbours);
 }
-
-//Progression 2 - create a formation object and return it
-var formation = [4, 4, 3];
-
-//write your function here
-
-// Dont edit the following code
-
-try {
-  var formationObject = createFormation(formation);
-} catch (e) {
-  //do nothing
+//Progression 2:
+//Check if the data given is of the right type
+//parents = String, noOfSiblings = Number, isNuclearFamily = Boolean
+function moreAboutKaren(parents, noOfSiblings, isNuclearFamily){
+    if(typeof(parents)==="string" && typeof(noOfSiblings)==="number" && typeof(isNuclearFamily)==="boolean")
+    return true;
+return false;
 }
+//Progression 3:
+//Lily is suspicious about Karen's new friend
+//Karen tells her friend's age and even writes it down
+//Check which one those is not a number (NaN) and return that value
+function doesFriendExist(ageInText, ageInNumber){
+    if(isNaN(ageInText))
+    return ageInText;
+if(isNaN(ageInNumber))
+  return ageInNumber;
+}
+//Progression 4:
+//Lily gave Karen x sweets
+//Karen ate y sweets herself
+//On her way to the river, she ate another z sweets every n meters travelled
+//Her friend divided the remaining sweets into 2 parts for each
+//How many sweets did her friend get to eat?
+function sweetTooth(totalNoOfSweets, sweetsConsumedByKaren, sweetsConsumedInNMeters, metersToTravel){
+        if(typeof(totalNoOfSweets)!=="number" || typeof(sweetsConsumedByKaren)!=="number" || typeof(sweetsConsumedInNMeters)!=="number" || typeof(metersToTravel)!=="number")
+         return `No sweets for Karen's friend`;
+        if(totalNoOfSweets===0 && sweetsConsumedByKaren===0 &&  sweetsConsumedInNMeters===0 && metersToTravel===0)
+           return 0;
+        if(totalNoOfSweets===(sweetsConsumedByKaren+(sweetsConsumedInNMeters*metersToTravel)))
+           return 0;
+    }
 
-//Progression 3 - Filter players that debuted in ___ year
-
-//Progression 4 - Filter players that play at the position _______
-
-//Progression 5 - Filter players that have won ______ award
-
-//Progression 6 - Filter players that won ______ award ____ times
-
-//Progression 7 - Filter players that won ______ award and belong to ______ country
-
-//Progression 8 - Filter players that won atleast ______ awards, belong to ______ team and are younger than ____
-
-//Progression 9 - Sort players in descending order of their age
-
-//Progression 10 - Sort players beloging to _____ team in descending order of awards won
-
-//Challenge 1 - Sort players that have won _______ award _____ times and belong to _______ country in alphabetical order of their names
-
-//Challenge 2 - Sort players that are older than _____ years in alphabetical order
-//Sort the awards won by them in reverse chronological order
+//Progression 5:
+//As Lily moves closer, it gets colder. She checks the temperature on her mobile
+//It only shows in fahrenheit. Convert the data to celsius and return it.
+function convertToCelsius(fahrenheit){
+     return (typeof(fahrenheit)!=='number')?"Technical Error!":(fahrenheit-32)/1.8;
+}
+//Progression 6:
+//Lily can now do multiple things to deal with this
+//1. Take her daughter to a doctor
+//2. Talk to her husband about it
+//3. Counsel her daughter herself
+//4. Lock her daughter in her room
+//Given a value, return which of these above actions Lily would take
+function aDifficultChoice(choice){
+    if(choice===1)
+    return `Take her daughter to a doctor`;
+   else if(choice===-1)
+    return `Break down and give up all hope`;
+   else if(choice===undefined)
+    return `Wasn't able to decide`;
+   else 
+    return `Refused to do anything for Karen`;
+}
+//Progression 7:
+//Lily realized that she'd hurt her daughter
+//All she wants now is for her to stop crying
+//She refuses to talk to her but Lily doesn't stop trying
+//She tries out multiple things hoping for the best
+//Take all of Lily's strategies and concatenate them to a single var
+//Seperate the strategies by a single space
+//Return the length of the complete strategy
